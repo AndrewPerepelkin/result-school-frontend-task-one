8ea1a18e23d3b37ed716c4e842f14d3b21233b3f
AndrewPerepelkin <avp.tep@rambler.ru>
2022-09-12 18:17:37 +0300

    refactor: added searchStatus component

--- fast-company/src/App.js
+++ fast-company/src/App.js
@@ -1,33 +1,16 @@
 import React, {useState} from 'react';
 import api from './api';
 import UsersData from './components/usersData';
+import SearchStatus from './components/searchStatus';
 
 const App = () => {
   const [users, setUsers] = useState(api.users.fetchAll());
   
-  const renderPhrase = (n) => {
-    if (n === 0) {
-      return 'Никто с тобой не тусанет';
-    } else if (String(n).endsWith('1')) {
-      return n + ' человек тусанет с тобой сегодня';
-    } else if ((n < 10 || n > 20) && (String(n).endsWith('2') || String(n).endsWith('3') || String(n).endsWith('4'))) {
-      return n + ' человека тусанет с тобой сегодня';
-    } else {
-      return n + ' человек тусанет с тобой сегодня';
-    }
-  }
-
   const handleDelete = (id) => setUsers(prevState => prevState.filter(user => user._id !== id))
 
   return (
     <>
-      <h2>
-        <span className={(users.length === 0) ? 
-          'badge bg-danger' : 
-          'badge bg-primary'}>
-          {renderPhrase(users.length)}
-        </span>
-      </h2>
+      <SearchStatus usersNumber={users.length} />
 
       {!!users.length && 
       
