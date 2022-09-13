1255846c1e97b94e888016073ec9c2039519aebc
AndrewPerepelkin <avp.tep@rambler.ru>
2022-09-13 12:39:21 +0300

    refactor: refactored toggle bookmark function

--- fast-company/src/App.js
+++ fast-company/src/App.js
@@ -4,18 +4,9 @@ import Users from './components/users';
 import SearchStatus from './components/searchStatus';
 
 const App = () => {
-  const [users, setUsers] = useState(api.users.fetchAll());
+  const [users, setUsers] = useState(() => api.users.fetchAll());
   const handleDelete = (id) => setUsers(prevState => prevState.filter(user => user._id !== id));
-  const handleToggelBookMark = (id) => {
-    const updateUsers = users.map(user => {
-      if (user._id === id) {
-        user.bookmark = !user.bookmark;
-      }
-      return user;
-    });
-    setUsers(updateUsers);
-  }
-
+  const handleToggelBookMark = (id) => setUsers(users.map(user => user._id === id ? {...user, bookmark: !user.bookmark} : user));
   return (
     <>
       <SearchStatus usersNumber={users.length} />
