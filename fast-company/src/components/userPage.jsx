07b5806e9cebc24853847f110c7f106c25880599
AndrewPerepelkin <avp.tep@rambler.ru>
2022-10-12 06:52:52 +0300

    fix: fixed warn in user Page

--- fast-company/src/components/userPage.jsx
+++ fast-company/src/components/userPage.jsx
@@ -1,25 +1,25 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom';
+import {useHistory} from 'react-router-dom';
 import PropTypes from 'prop-types';
 import QualitiesList from './qualitiesList';
 
-const UserPage = ({ user }) => {
+const UserPage = ({user}) => {
   const history = useHistory();
   const handleReturn = () => history.push('/users');
 
   return (
     <>
-      {user
-        ? (<>
-            <h2>{user.name}</h2>
-            <h4>Профессия: {user.profession.name}</h4>
-            <div>{<QualitiesList qualities={user.qualities} />}</div>
-            <div>Встретился {user.completedMeetings} раз</div>
-            <div>Оценка: {user.rate}</div>
-          </>)
-        : (
+      {user ? (
+        <>
+          <h2>{user.name}</h2>
+          <h4>Профессия: {user.profession.name}</h4>
+          <div>{<QualitiesList qualities={user.qualities} />}</div>
+          <div>Встретился {user.completedMeetings} раз</div>
+          <div>Оценка: {user.rate}</div>
+        </>
+      ) : (
         <div>Loading...</div>
-          )}
+      )}
       <button
         className='btn btn-secondary mt-2'
         onClick={() => handleReturn()}
@@ -31,7 +31,7 @@ const UserPage = ({ user }) => {
 };
 
 UserPage.propTypes = {
-  user: PropTypes.object.isRequired
+  user: PropTypes.object
 };
 
 export default UserPage;
