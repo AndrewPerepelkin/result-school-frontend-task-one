64440b2660ad12a0c8a56b94488f83c0f27c09cc
AndrewPerepelkin <avp.tep@rambler.ru>
2022-10-24 16:54:02 +0300

    feat: added textFeild componett in login form

--- fast-company/src/layouts/login.jsx
+++ fast-company/src/layouts/login.jsx
@@ -1,22 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
+import TextFied from '../components/textFied';
 
 const Login = () => {
+  const [data, setData] = useState({email: '', password: ''});
+
+  const handleChange = ({target}) => {
+    setData((prevState) => ({...prevState, [target.name]: target.value}));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(data);
+  };
+
   return (
-    <form action=''>
-      <div>
-        <label htmlFor='email'>Электронная почта</label>
-        <input
-          type='text'
-          id='email'
-        />
-      </div>
-      <div>
-        <label htmlFor='password'>Пароль</label>
-        <input
-          type='password'
-          id='password'
-        />
-      </div>
+    <form onSubmit={handleSubmit}>
+      <TextFied
+        label={'Электронная почта'}
+        name={'email'}
+        value={data.email}
+        onChange={handleChange}
+      />
+      <TextFied
+        label={'Пароль'}
+        type={'password'}
+        name={'password'}
+        value={data.password}
+        onChange={handleChange}
+      />
+      <button type='submit'>Submit</button>
     </form>
   );
 };
