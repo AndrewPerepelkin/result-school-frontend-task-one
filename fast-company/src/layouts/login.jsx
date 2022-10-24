6fa4a5d7711bd8c6b9bf5932c3ad984f57c53f18
AndrewPerepelkin <avp.tep@rambler.ru>
2022-10-24 15:34:39 +0300

    feat: added login form

--- fast-company/src/layouts/login.jsx
+++ fast-company/src/layouts/login.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
 
 const Login = () => {
-  return <h1>Login</h1>;
+  return (
+    <form action=''>
+      <div>
+        <label htmlFor='email'>Электронная почта</label>
+        <input
+          type='text'
+          id='email'
+        />
+      </div>
+      <div>
+        <label htmlFor='password'>Пароль</label>
+        <input
+          type='password'
+          id='password'
+        />
+      </div>
+    </form>
+  );
 };
 
 export default Login;
