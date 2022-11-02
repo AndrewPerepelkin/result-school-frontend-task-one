dad0641788ded92ceaccac48e0f84200c9fe3bfc
AndrewPerepelkin <avp.tep@rambler.ru>
2022-11-02 04:33:59 +0300

    feat: created edit user page

---
+++ fast-company/src/components/page/userPageEdit/userPageEdit.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+// import PropTypes from 'prop-types'
+
+const UserPageEdit = (props) => {
+  return <div>UserPageEdit</div>;
+};
+
+UserPageEdit.propTypes = {};
+
+export default UserPageEdit;
