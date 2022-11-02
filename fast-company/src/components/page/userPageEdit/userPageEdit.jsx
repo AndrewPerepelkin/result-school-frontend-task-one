7cb8f69a18df03a17a68558fcc14262fcab960a9
AndrewPerepelkin <avp.tep@rambler.ru>
2022-11-02 07:20:56 +0300

    feat: added update form component

--- fast-company/src/components/page/userPageEdit/userPageEdit.jsx
+++ fast-company/src/components/page/userPageEdit/userPageEdit.jsx
@@ -2,20 +2,13 @@ import React, {useState, useEffect} from 'react';
 import PropTypes from 'prop-types';
 import api from '../../../api';
 import UpdateForm from '../../ui/updateForm';
-import {useHistory} from 'react-router-dom';
 
 const UserPageEdit = ({userId}) => {
   const [user, setUser] = useState();
-  const history = useHistory();
 
   useEffect(() => {
     api.users.getById(userId).then((data) => setUser(data));
   }, []);
-
-  const handleUpate = (id, data) => {
-    // api.users.update(id, data);
-    history.push(`/users/${userId}`);
-  };
 
   return (
     <>
@@ -29,15 +22,6 @@ const UserPageEdit = ({userId}) => {
                 </h3>
 
                 <UpdateForm user={user} />
-                <div className='d-flex flex-column align-items-center'>
-                  <button
-                    role='button'
-                    onClick={handleUpate}
-                    className='btn btn-primary w-100 mx-auto mb-2'
-                  >
-                    Обновить
-                  </button>
-                </div>
               </>
             ) : (
               <div className='text-center mb-4'>Загрузка...</div>
--- fast-company/src/components/ui/updateForm.jsx
+++ fast-company/src/components/ui/updateForm.jsx
@@ -1,8 +1,161 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import PropTypes from 'prop-types';
+import TextField from '../common/form/textField';
+import SelectField from '../common/form/selectField';
+import RadioField from '../common/form/radioField';
+import MultiSelectField from '../common/form/multiSelectField';
+import api from '../../api/';
+import {useHistory} from 'react-router-dom';
 
 const UpdateForm = ({user}) => {
-  return <div>{user.name}</div>;
+  const getDefaultQualities = (data) => {
+    const QualitiesList = Object.keys(data).map((optionName) => ({
+      label: data[optionName].name,
+      value: data[optionName]._id,
+      color: data[optionName].color
+    }));
+    return QualitiesList;
+  };
+
+  const getDefaultProfession = (data) => {
+    return data._id;
+  };
+
+  const [data, setData] = useState({
+    email: user.email,
+    name: user.name,
+    profession: getDefaultProfession(user.profession),
+    sex: user.sex,
+    qualities: getDefaultQualities(user.qualities)
+  });
+  const [professions, setProfessions] = useState([]);
+  const [qualities, setQualities] = useState([]);
+  const history = useHistory();
+
+  useEffect(() => {
+    api.professions.fetchAll().then((data) => {
+      const professionsList = Object.keys(data).map((professionName) => ({
+        label: data[professionName].name,
+        value: data[professionName]._id
+      }));
+      setProfessions(professionsList);
+    });
+
+    api.qualities.fetchAll().then((data) => {
+      const QualitiesList = Object.keys(data).map((optionName) => ({
+        label: data[optionName].name,
+        value: data[optionName]._id,
+        color: data[optionName].color
+      }));
+      setQualities(QualitiesList);
+    });
+  }, []);
+
+  const handleChange = (target) => {
+    setData((prevState) => ({...prevState, [target.name]: target.value}));
+  };
+
+  const getProfessionById = (id) => {
+    for (const prof of professions) {
+      if (prof.value === id) {
+        return {_id: prof.value, name: prof.label};
+      }
+    }
+  };
+
+  const getQualities = (elements) => {
+    const qualitiesArray = [];
+    for (const elem of elements) {
+      for (const qual in qualities) {
+        if (elem.value === qualities[qual].value) {
+          qualitiesArray.push({
+            _id: qualities[qual].value,
+            name: qualities[qual].label,
+            color: qualities[qual].color
+          });
+        }
+      }
+    }
+    return qualitiesArray;
+  };
+
+  const handleUpdate = (e) => {
+    e.preventDefault();
+    const {profession, qualities} = data;
+    const updatedData = {
+      ...user,
+      ...data,
+      profession: getProfessionById(profession),
+      qualities: getQualities(qualities)
+    };
+    api.users.update(user._id, updatedData);
+    history.push(`/users/${user._id}`);
+  };
+
+  const handleCancel = () => {
+    history.replace(`/users/${user._id}`);
+  };
+
+  return (
+    <form onSubmit={handleUpdate}>
+      <TextField
+        label={'Имя:'}
+        name={'name'}
+        value={data.name}
+        onChange={handleChange}
+        // error={errors.email}
+      />
+      <TextField
+        label={'Электронная почта:'}
+        name={'email'}
+        value={data.email}
+        onChange={handleChange}
+        // error={errors.email}
+      />
+      <SelectField
+        label='Выберите профессию:'
+        name='profession'
+        value={data.profession}
+        defaultOption='Выберите профессию...'
+        onChange={handleChange}
+        options={professions}
+        // error={errors.profession}
+      />
+      <RadioField
+        options={[
+          {name: 'Муж', value: 'male'},
+          {name: 'Жен', value: 'female'}
+        ]}
+        value={data.sex}
+        name='sex'
+        onChange={handleChange}
+        label='Укажите Ваш пол:'
+      />
+      <MultiSelectField
+        name='qualities'
+        options={qualities}
+        onChange={handleChange}
+        label='Выберите качества:'
+        defaultValue={data.qualities}
+      />
+      <div className='d-flex flex-column align-items-center'>
+        <button
+          type='submit'
+          // disabled={!isValid}
+          className='btn btn-primary w-100 mx-auto mb-2'
+        >
+          Обновить
+        </button>
+        <button
+          type='button'
+          className='btn btn-secondary w-100 mx-auto mb-2'
+          onClick={handleCancel}
+        >
+          Отмена
+        </button>
+      </div>
+    </form>
+  );
 };
 
 UpdateForm.propTypes = {
