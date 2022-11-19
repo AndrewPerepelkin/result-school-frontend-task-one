c123c48d51aedcba97181e7d09c44168d0ccb2ff
AndrewPerepelkin <avp.tep@rambler.ru>
2022-11-19 21:23:18 +0300

    feat: added displaying userPage according docs

--- fast-company/src/components/page/userPage/userPage.jsx
+++ fast-company/src/components/page/userPage/userPage.jsx
@@ -11,34 +11,84 @@ const UserPage = ({id}) => {
   }, []);
 
   const history = useHistory();
-  const handleReturn = () => history.push('/users');
+  // const handleReturn = () => history.push('/users');
   const handleEdit = () => history.push(history.location.pathname + '/edit');
 
   return (
-    <div className='m-3'>
-      {user ? (
-        <div className='mb-3'>
-          <h2>{user.name}</h2>
-          <h4>Профессия: {user.profession.name}</h4>
-          <div>{<Qualities qualities={user.qualities} />}</div>
-          <div>Встретился {user.completedMeetings} раз</div>
-          <div>Оценка: {user.rate}</div>
-        </div>
-      ) : (
-        <div className='m-3'>Loading...</div>
-      )}
-      <button
-        className='btn btn-secondary m-2'
-        onClick={() => handleReturn()}
-      >
-        Все пользователи
-      </button>
-      <button
-        className='btn btn-primary'
-        onClick={() => handleEdit()}
-      >
-        Изменить
-      </button>
+    <div className='container'>
+      <div className='row gutters-sm'>
+        {user ? (
+          <div className='col-md-4 mb-3'>
+            <div className='card mb-3'>
+              <div className='card-body'>
+                <div className='d-flex flex-column align-items-center text-center position-relative'>
+                  <img
+                    src={`https://avatars.dicebear.com/api/avataaars/${(
+                      Math.random() + 1
+                    )
+                      .toString(36)
+                      .substring(7)}.svg`}
+                    className='rounded-circle shadow-1-strong me-3'
+                    alt='avatar'
+                    width='65'
+                    height='65'
+                  />
+                  <div className='mt-3'>
+                    <h4>{user.name}</h4>
+                    <p className='text-secondary mb-1'>
+                      {user.profession.name}
+                    </p>
+                    <div className='text-muted'>
+                      <i
+                        className='bi bi-caret-down-fill text-primary'
+                        role='button'
+                      ></i>
+                      <i
+                        className='bi bi-caret-up text-secondary'
+                        role='button'
+                      ></i>
+                      <span className='ms-2'>{user.rate}</span>
+                    </div>
+                  </div>
+                </div>
+                <button
+                  className='position-absolute top-0 end-0 btn btn-light btn-sm'
+                  onClick={() => handleEdit()}
+                >
+                  <i className='bi bi-gear'></i>
+                </button>
+              </div>
+
+              {/* <button
+                className='btn btn-secondary m-2'
+                onClick={() => handleReturn()}
+              >
+                Все пользователи
+              </button> */}
+            </div>
+            <div className='card mt-3'>
+              <div className='card-body d-flex flex-column justify-content-center text-center'>
+                <h5 className='card-title'>
+                  <span>Qualities</span>
+                </h5>
+                <p className='card-text'>
+                  {<Qualities qualities={user.qualities} />}
+                </p>
+              </div>
+            </div>
+            <div className='card mt-3'>
+              <div className='card-body d-flex flex-column justify-content-center text-center'>
+                <h5 className='card-title'>
+                  <span>Completed meetings</span>
+                </h5>
+                <h1 className='display-1'>{user.completedMeetings}</h1>
+              </div>
+            </div>
+          </div>
+        ) : (
+          <div className='m-3'>Loading...</div>
+        )}
+      </div>
     </div>
   );
 };
