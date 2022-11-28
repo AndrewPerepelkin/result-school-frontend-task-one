261b95a6a1a0bc7820271294f99c02858d8bf4be
AndrewPerepelkin <avp.tep@rambler.ru>
2022-11-28 07:04:07 +0300

    feat: added initialize of database to main page

--- fast-company/src/layouts/main.jsx
+++ fast-company/src/layouts/main.jsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import useMockData from '../utils/mockData';
 
 const Main = () => {
-  return <h1>Main</h1>;
+  const {error, status, progress, initialize} = useMockData();
+  const handleClick = () => {
+    initialize();
+  };
+  return (
+    <div className='container mt-5'>
+      <h1>Главная страница</h1>
+      <h3>Инициализация данных в FireBase</h3>
+      <ul className='list-group'>
+        <li className='list-group-item'>Статус: {status}</li>
+        <li className='list-group-item'>Выполнено: {progress}%</li>
+        {error && <li className='list-group-item'>Ошибка: {error}</li>}
+      </ul>
+      <button
+        className='btn btn-primary mt-3'
+        onClick={handleClick}
+      >
+        Инициализировать
+      </button>
+    </div>
+  );
 };
 
 export default Main;
