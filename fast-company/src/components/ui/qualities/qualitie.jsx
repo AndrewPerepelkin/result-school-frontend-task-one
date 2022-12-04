717e7b44ad59f0e5450caf342fdb383f2e9c4d6f
AndrewPerepelkin <avp.tep@rambler.ru>
2022-12-05 02:16:58 +0300

    fix: fixed displaying qualities loading message

--- fast-company/src/components/ui/qualities/qualitie.jsx
+++ fast-company/src/components/ui/qualities/qualitie.jsx
@@ -2,17 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {useQualities} from '../../../hooks/useQualities';
 const Qualitie = ({id}) => {
-  const {isLoading, getQuality} = useQualities();
-  const qual = getQuality(id);
-  return (
-    <>
-      {!isLoading ? (
-        <span className={`badge m-1 bg-${qual.color}`}>{qual.name}</span>
-      ) : (
-        <span>загрузка...</span>
-      )}
-    </>
-  );
+  const {getQuality} = useQualities();
+  const {color, name} = getQuality(id);
+  return <span className={`badge m-1 bg-${color}`}>{name}</span>;
 };
 
 Qualitie.propTypes = {
--- fast-company/src/components/ui/qualities/qualitiesList.jsx
+++ fast-company/src/components/ui/qualities/qualitiesList.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Qualitie from './qualitie';
+import {useQualities} from '../../../hooks/useQualities';
 
 const QualitiesList = ({qualities}) => {
+  const {isLoading} = useQualities();
   return (
     <>
-      {qualities.map((q) => (
-        <Qualitie
-          key={q}
-          id={q}
-        />
-      ))}
+      {!isLoading ? (
+        qualities.map((q) => (
+          <Qualitie
+            key={q}
+            id={q}
+          />
+        ))
+      ) : (
+        <span>загрузка...</span>
+      )}
     </>
   );
 };
