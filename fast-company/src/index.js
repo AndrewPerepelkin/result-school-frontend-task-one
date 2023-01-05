8d1d4c2b747cfe35cc4b633bb81e54e36a830b0c
AndrewPerepelkin <avp.tep@rambler.ru>
2023-01-05 22:35:59 +0300

    feat: added redux and created store

--- fast-company/src/index.js
+++ fast-company/src/index.js
@@ -6,11 +6,17 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
+import {createStore} from './store/createStore';
+import {Provider} from 'react-redux';
+
+const store = createStore();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </BrowserRouter>
 );
 
