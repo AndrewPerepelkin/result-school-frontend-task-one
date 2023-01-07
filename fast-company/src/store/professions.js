ce53596a8ae6fd5887a8acf62fe8cba8e669fbe8
AndrewPerepelkin <avp.tep@rambler.ru>
2023-01-07 04:47:01 +0300

    feat: created professions slice, actions, reducer

---
+++ fast-company/src/store/professions.js
@@ -0,0 +1,56 @@
+import {createSlice} from '@reduxjs/toolkit';
+import professionService from '../services/professionService';
+
+const professionsSlice = createSlice({
+  name: 'professions',
+  initialState: {
+    entities: null,
+    isLoading: true,
+    error: null,
+    lastFetch: null
+  },
+  reducers: {
+    professionsRequested: (store) => {
+      store.isLoading = true;
+    },
+    professionsReceived: (store, action) => {
+      store.entities = action.payload;
+      store.lastFetch = Date.now();
+      store.isLoading = false;
+    },
+    professionsRequestFailed: (store, action) => {
+      store.error = action.payload;
+      store.isLoading = false;
+    }
+  }
+});
+
+const {reducer: professionsReducer, actions} = professionsSlice;
+const {professionsRequested, professionsReceived, professionsRequestFailed} =
+  actions;
+
+function isOutdated(date) {
+  return Date.now() - date > 10 * 60 * 1000;
+}
+
+export const loadProfessionsList = () => async (dispatch, getState) => {
+  const {lastFetch} = getState().professions;
+  if (isOutdated(lastFetch)) {
+    dispatch(professionsRequested());
+    try {
+      const {content} = await professionService.get();
+      dispatch(professionsReceived(content));
+    } catch (error) {
+      dispatch(professionsRequestFailed(error.message));
+    }
+  }
+};
+
+export const getProfessions = () => (state) => state.professions.entities;
+export const getProfessionsLoadingStatus = () => (state) =>
+  state.professions.isLoading;
+export const getProfessionById = (professionId) => (state) => {
+  return state.professions.entities.find((p) => p._id === professionId);
+};
+
+export default professionsReducer;
