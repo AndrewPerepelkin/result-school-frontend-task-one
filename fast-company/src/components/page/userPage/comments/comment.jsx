9e75a0b8e3eedd6fde7eaa7ba83fc5c3d5c598d1
AndrewPerepelkin <avp.tep@rambler.ru>
2023-01-10 01:18:59 +0300

    refactor: change time function, used user selector

--- fast-company/src/components/page/userPage/comments/comment.jsx
+++ fast-company/src/components/page/userPage/comments/comment.jsx
@@ -1,42 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {useSelector} from 'react-redux';
-import {useAuth} from '../../../../hooks/useAuth';
-import {getUserById} from '../../../../store/users';
+import {getCurrentUserId, getUserById} from '../../../../store/users';
+import getTimeFormat from '../../../../utils/getTimeFormat';
 
 const Comment = ({userId, created_at: time, _id, content, onRemove}) => {
   const user = useSelector(getUserById(userId));
-  const {currentUser} = useAuth();
-
-  const getCommentTime = (timestamp) => {
-    const date = new Date(parseInt(timestamp));
-    const now = new Date();
-    const yearDif = now.getFullYear() - date.getFullYear();
-    if (yearDif === 0) {
-      const dayDif = now.getDate() - date.getDate();
-      if (dayDif === 0) {
-        const hourDif = now.getHours() - date.getHours();
-        if (hourDif === 0) {
-          const minutesDif = now.getMinutes() - date.getMinutes();
-
-          if (minutesDif >= 0 && minutesDif < 5) return '1 минуту назад';
-          if (minutesDif >= 5 && minutesDif < 10) return '5 минут назад';
-          if (minutesDif >= 10 && minutesDif < 30) {
-            return '10 минут назад';
-          }
-          return '30 минут назад';
-        }
-        return `${date.getHours()}:${date.getMinutes()}`;
-      }
-
-      return `${date.getDate()} ${date.toLocaleString('default', {
-        month: 'long'
-      })}`;
-    }
-    return (
-      date.getFullYear() + '.' + (date.getMonth() + 1) + '.' + date.getDate()
-    );
-  };
+  const currentUserId = useSelector(getCurrentUserId());
 
   return (
     <div className='bg-light card-body mb-3'>
@@ -55,9 +25,9 @@ const Comment = ({userId, created_at: time, _id, content, onRemove}) => {
                 <div className='d-flex justify-content-between align-items-center'>
                   <p className='mb-1 '>
                     {user ? user.name : ' '}
-                    <span className='small'> - {getCommentTime(time)}</span>
+                    <span className='small'> - {getTimeFormat(time)}</span>
                   </p>
-                  {currentUser._id === userId && (
+                  {currentUserId === userId && (
                     <button
                       className='btn btn-sm text-primary d-flex align-items-center'
                       onClick={() => onRemove(_id)}
