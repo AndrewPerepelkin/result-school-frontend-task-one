70640d7f8b0bffeb1650f40838b50d7df7ab0db8
AndrewPerepelkin <avp.tep@rambler.ru>
2023-01-09 20:47:12 +0300

    refactor: used Router and custom history

--- fast-company/src/index.js
+++ fast-company/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {BrowserRouter} from 'react-router-dom';
+import {Router} from 'react-router-dom';
 
 import './index.css';
 import reportWebVitals from './reportWebVitals';
@@ -8,16 +8,17 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 import {createStore} from './store/createStore';
 import {Provider} from 'react-redux';
+import history from './utils/history';
 
 const store = createStore();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <Router history={history}>
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>
+  </Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
